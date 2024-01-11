dce7e140ce8cd721fc022fd53502b827276d00c7
Lucas Schmidt <lucas.schmidt015@gmail.com>
2024-01-10 23:20:00 -0300

    Implemented the first model.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+//External packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./utils/database");
 const path = require("path");
 const cors = require("cors");
 
+//routes
+const userRouter = require("./routes/user");
+
+//models
+const userModel = require("./models/user");
+
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "resources")));
+
+app.use(userRouter);
 
 sequelize
   .sync({ force: false })
---
+++ models/user.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
+
+const User = sequelize.define("User", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING(150),
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
